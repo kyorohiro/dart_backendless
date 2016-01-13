2ecdb1730c09bcbcb20147ff6105499aadc0ca1c
kyorohiro <kyorohiro@gmail.com>
2016-01-14 02:42:42 +0900

    add registi logout func

--- lib/backendless.dart
+++ lib/backendless.dart
@@ -10,48 +10,111 @@ class Backendless {
   String secretKey;
   Backendless(this.builder, this.applicationId, this.secretKey) {}
 
-  Future<LoginResult> login(String name, String pass, {String version: "v1"}) async {
+  Future<LoginResult> login(String login, String password, {String version: "v1"}) async {
     TinyNetRequester requester = await this.builder.createRequester();
-    TinyNetRequesterResponse resonse = await requester.request(TinyNetRequester.TYPE_POST, "https://api.backendless.com/${version}/users/login", headers: {"application-id": applicationId, "secret-key": secretKey, "application-type": "REST", "Content-Type": "application/json"});
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_POST, //
+        "https://api.backendless.com/${version}/users/login", //
+        headers: {
+          "application-id": applicationId, //
+          "secret-key": secretKey, //
+          "application-type": "REST", //
+          "Content-Type": "application/json" //
+        }, //
+        data: """{"login" : ${login}, "password" : ${password}}""" //
+        );
     return new LoginResult.fromResponse(resonse);
   }
-/*
-  Future<CurlResult> regist(String name, String pass) async {
-    Curl curl = new Curl();
-    CurlResult r = await curl.post("https://api.backendless.com/v1/users/register",
-    """{"password":"${pass}", "email":"${name}"}""",
-    headers: {
-      "application-id": "",
-      "secret-key": "",
-      "application-type": "REST",
-      "Content-Type":"application/json"});
-    if (r.buffer != null && r.status == 200) {
-      print("##1# ${r.status} ${UTF8.decode(r.buffer.asUint8List())}");
-    } else {
-      print("##2# ${r.status}");
-    }
-    return r;
+
+  Future<RegistResult> regist(Map<String, String> properties, {String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_POST, //
+        "https://api.backendless.com/${version}/users/register", //
+        headers: {
+          "application-id": applicationId, //
+          "secret-key": secretKey, //
+          "application-type": "REST", //
+          "Content-Type": "application/json" //
+        }, //
+        data: JSON.encode(properties) //
+        );
+    return new RegistResult.fromResponse(resonse);
   }
 
-  Future<CurlResult> logout(String userToken) async {
-    print("###${userToken}");
-    MyStatus.instance.userToken = "";
-    Curl curl = new Curl();
-    CurlResult r = await curl.get("https://api.backendless.com/v1/users/logout",
-    headers: {
-       "application-id": "",
-       "secret-key": "",
-       "application-type": "REST",
-       "user-token": "${userToken}",
-       "Content-Type":"application/json"});
-    if (r.buffer != null && r.status == 200) {
-      print("##1# ${r.status} ${UTF8.decode(r.buffer.asUint8List())}");
-    } else {
-      print("##2# ${r.status}");
-    }
-    return r;
+  Future<LogoutResult> logout(String userToken, {String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_GET, //
+        "https://api.backendless.com/${version}/users/logout", //
+        headers: {
+          "application-id": applicationId, //
+          "secret-key": secretKey, //
+          "application-type": "REST", //
+          "user-token": "${userToken}" //
+        } //
+    );
+    return new LogoutResult.fromResponse(resonse);
   }
-  */
+}
+
+class LogoutResult {
+  bool isOk = false;
+  String objectId = "";
+  String message = "";
+  int code = 9999;
+  Map keyValues = {};
+
+  LogoutResult.fromResponse(TinyNetRequesterResponse r) {
+    String message = UTF8.decode(r.response.asUint8List());
+
+    try {
+      keyValues = JSON.decode(message);
+    } catch (e) {}
+
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+
+    if (r.status == 200) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+  }
+}
+
+class RegistResult {
+  bool isOk = false;
+  String objectId = "";
+  String message = "";
+  int code = 9999;
+  Map keyValues = {};
+
+  RegistResult.fromResponse(TinyNetRequesterResponse r) {
+    String message = UTF8.decode(r.response.asUint8List());
+    try {
+      keyValues = JSON.decode(message);
+    } catch (e) {}
+    if (keyValues.containsKey("objectId")) {
+      objectId = keyValues["objectId"];
+    }
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+
+    if (r.status == 200) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+  }
 }
 
 class LoginResult {
@@ -60,25 +123,26 @@ class LoginResult {
   String objectId = "";
   String message = "";
   int code = 9999;
+  Map keyValues = {};
 
   LoginResult.fromResponse(TinyNetRequesterResponse r) {
     String message = UTF8.decode(r.response.asUint8List());
-    Map cont = {};
+
     try {
-      cont = JSON.decode(message);
+      keyValues = JSON.decode(message);
     } catch (e) {}
 
-    if (cont.containsKey("user-token")) {
-      userToken = cont["user-token"];
+    if (keyValues.containsKey("user-token")) {
+      userToken = keyValues["user-token"];
     }
-    if (cont.containsKey("objectId")) {
-      objectId = cont["objectId"];
+    if (keyValues.containsKey("objectId")) {
+      objectId = keyValues["objectId"];
     }
-    if (cont.containsKey("code")) {
-      code = cont["code"];
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
     }
-    if (cont.containsKey("message")) {
-      message = cont["message"];
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
     }
 
     if (r.status == 200 && userToken != null && userToken.length != 0) {
