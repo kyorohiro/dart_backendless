fcb1952b13638edf09858e9591faf49692c0f5f8
kyorohiro <kyorohiro@gmail.com>
2016-01-15 05:14:19 +0900

    extract user.dart from backendless

--- lib/backendless.dart
+++ lib/backendless.dart
@@ -4,67 +4,22 @@ import 'tinynet.dart';
 import 'dart:async';
 import 'dart:convert';
 
-class Backendless {
+part 'src/backendless/user.dart';
+
+class BackendlessData {
   TinyNetBuilder builder;
   String applicationId;
   String secretKey;
-  Backendless(this.builder, this.applicationId, this.secretKey) {}
-
-  Future<LoginResult> login(String login, String password, {String version: "v1"}) async {
+  BackendlessData(this.builder, this.applicationId, this.secretKey) {}
+  Future<SaveDataResult> saveData(String tableName, Map<String, Object> body, {String userToken: null, String version: "v1"}) async {
     TinyNetRequester requester = await this.builder.createRequester();
-    TinyNetRequesterResponse resonse = await requester.request(
-        TinyNetRequester.TYPE_POST, //
-        "https://api.backendless.com/${version}/users/login", //
-        headers: {
-          "application-id": applicationId, //
-          "secret-key": secretKey, //
-          "application-type": "REST", //
-          "Content-Type": "application/json" //
-        }, //
-        data: """{"login" : "${login}", "password" : "${password}"}""" //
-        );
-    return new LoginResult.fromResponse(resonse);
-  }
-  Future<RegistResult> regist(Map<String, String> properties, {String version: "v1"}) async {
-    TinyNetRequester requester = await this.builder.createRequester();
-    TinyNetRequesterResponse resonse = await requester.request(
-        TinyNetRequester.TYPE_POST, //
-        "https://api.backendless.com/${version}/users/register", //
-        headers: {
-          "application-id": applicationId, //
-          "secret-key": secretKey, //
-          "application-type": "REST", //
-          "Content-Type": "application/json" //
-        }, //
-        data: JSON.encode(properties) //
-        );
-    return new RegistResult.fromResponse(resonse);
-  }
-
-  Future<LogoutResult> logout(String userToken, {String version: "v1"}) async {
-    TinyNetRequester requester = await this.builder.createRequester();
-    TinyNetRequesterResponse resonse = await requester.request(
-        TinyNetRequester.TYPE_GET, //
-        "https://api.backendless.com/${version}/users/logout", //
-        headers: {
-          "application-id": applicationId, //
-          "secret-key": secretKey, //
-          "application-type": "REST", //
-          "user-token": "${userToken}" //
-        } //
-    );
-    return new LogoutResult.fromResponse(resonse);
-  }
-
-  Future<SaveDataResult> saveData(String tableName, Map<String,Object> body, {String userToken:null, String version: "v1"}) async {
-    TinyNetRequester requester = await this.builder.createRequester();
-    Map<String,String> headers = {
+    Map<String, String> headers = {
       "application-id": applicationId, //
       "secret-key": secretKey, //
       "application-type": "REST", //
       "Content-Type": "application/json" //
     };
-    if(userToken != null) {
+    if (userToken != null) {
       headers["user-token"] = userToken;
     }
     TinyNetRequesterResponse resonse = await requester.request(
@@ -72,8 +27,108 @@ class Backendless {
         "https://api.backendless.com/${version}/data/${tableName}", //
         headers: headers, //
         data: JSON.encode(body));
-    
+
     return new SaveDataResult.fromResponse(resonse);
+  }
+
+  Future<Object> retrieveSchemeDefinition(String tableName, Map<String, Object> body, {String userToken: null, String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    Map<String, String> headers = {
+      "application-id": applicationId, //
+      "secret-key": secretKey, //
+      "application-type": "REST", //
+      "Content-Type": "application/json" //
+    };
+    if (userToken != null) {
+      headers["user-token"] = userToken;
+    }
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_GET, //
+        "https://api.backendless.com/${version}/data/${tableName}/properties", //
+        headers: headers, //
+        data: JSON.encode(body));
+
+    return new SaveDataResult.fromResponse(resonse);
+  }
+}
+
+class RetrieveSchemeDefinitionResultItem {
+  bool autoLoad = null;
+  String customRegex = null;
+  String defaultValue = null;
+  bool isPrimaryKey = null;
+  String name = null;
+  String relatedTable = null;
+  bool required = null;
+  String type = null;
+  RetrieveSchemeDefinitionResultItem.fromMap(Map m) {
+    var t = null;
+    t = m["autoLoad"];
+    autoLoad = (t is bool ? t : null);
+
+    t = m["customRegex"];
+    customRegex = (t is String ? t : null);
+
+    t = m["defaultValue"];
+    defaultValue = (t is String ? t : null);
+
+    t = m["isPrimaryKey"];
+    isPrimaryKey = (t is bool ? t : null);
+
+    t = m["name"];
+    name = (t is String ? t : null);
+
+    t = m["relatedTable"];
+    relatedTable = (t is String ? t : null);
+
+    t = m["required"];
+    required = (t is bool ? t : null);
+
+    t = m["type"];
+    type = (t is String ? t : null);
+  }
+}
+
+class RetrieveSchemeDefinitionResult {
+  bool isOk = false;
+
+  //
+  String message = "";
+  int code = 9999;
+  Map keyValues = {};
+  List<Map<Object, Object>> columnSources = [];
+  List<RetrieveSchemeDefinitionResultItem> columns = [];
+  int statusCode = 0;
+  RetrieveSchemeDefinitionResult.fromResponse(TinyNetRequesterResponse r) {
+    String utf8binary = UTF8.decode(r.response.asUint8List());
+
+    statusCode = r.status;
+    if (r.status == 200) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+
+    try {
+      var t = JSON.decode(utf8binary);
+      if (t is Map) {
+        keyValues = JSON.decode(utf8binary);
+      }
+      if (t is List) {
+        columnSources = t;
+      }
+    } catch (e) {}
+
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+
+    for (Map m in columnSources) {
+      columns.add(new RetrieveSchemeDefinitionResultItem.fromMap(m));
+    }
   }
 }
 
@@ -126,107 +181,6 @@ class SaveDataResult {
     }
     if (keyValues.containsKey("___class")) {
       classId = keyValues["___class"];
-    }
-  }
-}
-
-class LogoutResult {
-  bool isOk = false;
-  String objectId = "";
-  String message = "";
-  int code = 9999;
-  Map keyValues = {};
-  int statusCode = 0;
-
-  LogoutResult.fromResponse(TinyNetRequesterResponse r) {
-    String utf8binary = UTF8.decode(r.response.asUint8List());
-
-    try {
-      keyValues = JSON.decode(utf8binary);
-    } catch (e) {}
-
-    if (keyValues.containsKey("code")) {
-      code = keyValues["code"];
-    }
-    if (keyValues.containsKey("message")) {
-      message = keyValues["message"];
-    }
-
-    statusCode = r.status;
-
-    if (r.status == 200) {
-      isOk = true;
-    } else {
-      isOk = false;
-    }
-  }
-}
-
-class RegistResult {
-  bool isOk = false;
-  String objectId = "";
-  String message = "";
-  int code = 9999;
-  int statusCode = 0;
-  Map keyValues = {};
-
-  RegistResult.fromResponse(TinyNetRequesterResponse r) {
-    String utf8binary = UTF8.decode(r.response.asUint8List());
-    try {
-      keyValues = JSON.decode(utf8binary);
-    } catch (e) {}
-    if (keyValues.containsKey("objectId")) {
-      objectId = keyValues["objectId"];
-    }
-    if (keyValues.containsKey("code")) {
-      code = keyValues["code"];
-    }
-    if (keyValues.containsKey("message")) {
-      message = keyValues["message"];
-    }
-    statusCode = r.status;
-    if (r.status == 200) {
-      isOk = true;
-    } else {
-      isOk = false;
-    }
-  }
-}
-
-class LoginResult {
-  bool isOk = false;
-  String userToken = "";
-  String objectId = "";
-  String message = "";
-  int code = 9999;
-  Map keyValues = {};
-  int statusCode = 0;
-
-  LoginResult.fromResponse(TinyNetRequesterResponse r) {
-    String utf8binary = UTF8.decode(r.response.asUint8List());
-
-    try {
-      keyValues = JSON.decode(utf8binary);
-    } catch (e) {}
-
-    if (keyValues.containsKey("user-token")) {
-      userToken = keyValues["user-token"];
-    }
-    if (keyValues.containsKey("objectId")) {
-      objectId = keyValues["objectId"];
-    }
-    if (keyValues.containsKey("code")) {
-      code = keyValues["code"];
-    }
-    if (keyValues.containsKey("message")) {
-      message = keyValues["message"];
-    }
-
-    statusCode = r.status;
-    if (r.status == 200 && userToken != null && userToken.length != 0) {
-      isOk = true;
-    } else {
-      isOk = false;
     }
   }
 }
---
+++ lib/src/backendless/user.dart
@@ -0,0 +1,158 @@
+part of hetima_mbaas_backendless;
+
+
+class BackendlessUser {
+  TinyNetBuilder builder;
+  String applicationId;
+  String secretKey;
+  BackendlessUser(this.builder, this.applicationId, this.secretKey) {}
+
+  Future<LoginResult> login(String login, String password, {String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_POST, //
+        "https://api.backendless.com/${version}/users/login", //
+        headers: {
+          "application-id": applicationId, //
+          "secret-key": secretKey, //
+          "application-type": "REST", //
+          "Content-Type": "application/json" //
+        }, //
+        data: """{"login" : "${login}", "password" : "${password}"}""" //
+        );
+    return new LoginResult.fromResponse(resonse);
+  }
+
+  Future<RegistResult> regist(Map<String, String> properties, {String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_POST, //
+        "https://api.backendless.com/${version}/users/register", //
+        headers: {
+          "application-id": applicationId, //
+          "secret-key": secretKey, //
+          "application-type": "REST", //
+          "Content-Type": "application/json" //
+        }, //
+        data: JSON.encode(properties) //
+        );
+    return new RegistResult.fromResponse(resonse);
+  }
+
+  Future<LogoutResult> logout(String userToken, {String version: "v1"}) async {
+    TinyNetRequester requester = await this.builder.createRequester();
+    TinyNetRequesterResponse resonse = await requester.request(
+        TinyNetRequester.TYPE_GET, //
+        "https://api.backendless.com/${version}/users/logout", //
+        headers: {
+      "application-id": applicationId, //
+      "secret-key": secretKey, //
+      "application-type": "REST", //
+      "user-token": "${userToken}" //
+    } //
+        );
+    return new LogoutResult.fromResponse(resonse);
+  }
+}
+
+
+class LogoutResult {
+  bool isOk = false;
+  String objectId = "";
+  String message = "";
+  int code = 9999;
+  Map keyValues = {};
+  int statusCode = 0;
+
+  LogoutResult.fromResponse(TinyNetRequesterResponse r) {
+    String utf8binary = UTF8.decode(r.response.asUint8List());
+
+    try {
+      keyValues = JSON.decode(utf8binary);
+    } catch (e) {}
+
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+
+    statusCode = r.status;
+
+    if (r.status == 200) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+  }
+}
+
+class RegistResult {
+  bool isOk = false;
+  String objectId = "";
+  String message = "";
+  int code = 9999;
+  int statusCode = 0;
+  Map keyValues = {};
+
+  RegistResult.fromResponse(TinyNetRequesterResponse r) {
+    String utf8binary = UTF8.decode(r.response.asUint8List());
+    try {
+      keyValues = JSON.decode(utf8binary);
+    } catch (e) {}
+    if (keyValues.containsKey("objectId")) {
+      objectId = keyValues["objectId"];
+    }
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+    statusCode = r.status;
+    if (r.status == 200) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+  }
+}
+
+class LoginResult {
+  bool isOk = false;
+  String userToken = "";
+  String objectId = "";
+  String message = "";
+  int code = 9999;
+  Map keyValues = {};
+  int statusCode = 0;
+
+  LoginResult.fromResponse(TinyNetRequesterResponse r) {
+    String utf8binary = UTF8.decode(r.response.asUint8List());
+
+    try {
+      keyValues = JSON.decode(utf8binary);
+    } catch (e) {}
+
+    if (keyValues.containsKey("user-token")) {
+      userToken = keyValues["user-token"];
+    }
+    if (keyValues.containsKey("objectId")) {
+      objectId = keyValues["objectId"];
+    }
+    if (keyValues.containsKey("code")) {
+      code = keyValues["code"];
+    }
+    if (keyValues.containsKey("message")) {
+      message = keyValues["message"];
+    }
+
+    statusCode = r.status;
+    if (r.status == 200 && userToken != null && userToken.length != 0) {
+      isOk = true;
+    } else {
+      isOk = false;
+    }
+  }
+}
