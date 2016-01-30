28abe57908d0e3cce4446c5e7fb7a4105b088595
kyorohiro <kyorohiro@gmail.com>
2016-01-31 06:36:19 +0900

    mod testcase

--- test_net/eqteset/lib/alltest.dart
+++ test_net/eqteset/lib/alltest.dart
@@ -9,8 +9,18 @@ kicktest(TinyNetBuilder builder) async {
     test("method", () async {
       TinyNetRequester requester = await builder.createRequester();
       TinyNetRequesterResponse response =
-      await requester.request(TinyNetRequester.TYPE_POST, "http://httpbin.org/post",headers: {"nono":"nano"},data: "hello!!");
+      await requester.request(
+        TinyNetRequester.TYPE_POST,//
+        "http://httpbin.org/post",//
+        headers: {
+          "nono":"nano",
+          "Content-Type": "application/json"},//
+        data: JSON.encode({"message":"hello!!"}));
       print("${UTF8.decode(response.response.asUint8List())}");
+      Map<String,Object> ret = JSON.decode(UTF8.decode(response.response.asUint8List()));
+      expect("application/json",(ret["headers"]as Map)["Content-Type"]);
+      expect("nano",(ret["headers"]as Map)["Nono"]);
+      expect("hello!!",(ret["json"]as Map)["message"]);
       return "";
     });
   });
