e2cc64e108d63e90d4ffe7c87ae32f901d58c9a2
kyorohiro <kyorohiro@gmail.com>
2016-03-05 22:24:09 +0900

    mod eqtest kicker for flutter ver

--- test_backendless/eqtest/lib/main.dart
+++ test_backendless/eqtest/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:umiuni2d_netbox/tinynet_flutter.dart';
 import 'package:test_backendless/alltests.dart' as te;
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+a() async {
+  new Future(() {
+    te.kicktests(new TinyNetFlutterBuilder());
+  });
+}
 
 void main() {
-  te.kicktests(new TinyNetFlutterBuilder());
+  runApp(new Center(child: new Text("Hello!!")));
+  a();
 }
