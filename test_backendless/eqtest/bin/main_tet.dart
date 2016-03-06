428e89955323add9e862237bd5b85fc7708b9711
kyorohiro <kyorohiro@gmail.com>
2016-03-06 19:41:15 +0900

    add backendless test on tetorica socket

---
+++ test_backendless/eqtest/bin/main_tet.dart
@@ -0,0 +1,8 @@
+import 'package:umiuni2d_netbox/tinynet_tetorica.dart';
+import 'package:tetorica/net_dartio.dart' as tet;
+import 'package:test_backendless/alltests.dart' as t;
+
+main() async {
+  TinyNetTetoricaBuilder builder = new TinyNetTetoricaBuilder(new tet.TetSocketBuilderDartIO());
+  t.kicktests(builder);
+}
